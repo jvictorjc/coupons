3dac210e5a26df455153c4036cf8a790fa7bb2ce
joao.carvalho <joao.carvalho@goias.gov.br>
2025-12-16 11:14:18 -0300

    Initial project coupons

---
+++ src/main/java/entities/Coupon.java
@@ -0,0 +1,28 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "coupons")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 6, unique = true)
+    private String code;
+    private String description;
+    private Double discountValue;
+    private LocalDateTime expirationDate;
+    private Boolean deleted;
+
+}
